e5dbf45735a4f98109d0ef9160a02b14436e2c38
Joe Kaiser <joe.kaiser@stackiq.com>
2017-05-05 15:35:27 -0600

    Fixed spreadsheet placement.

--- version.mk
+++ version.mk
@@ -1,5 +1,5 @@
 export ROLL	= stacki-docker
-export VERSION	= 17.03.0
-export ROLLVERSION = 4.0_$(shell date +%Y%m%d)
+export VERSION	= 17.03.1
+export ROLLVERSION = 4.0
 export RELEASE  = 7.x
 COLOR		= magenta
